86a19523bfdd1d86868e7e29431a89d1eed8505f
jdselsor <jdselsor@gmail.com>
2023-09-07 01:46:44 -0700

    set up cmake files

---
+++ CMakeFiles/ParakeetMetroidvania.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParakeetMetroidvania.dir/src/main.c.o"
+  "CMakeFiles/ParakeetMetroidvania.dir/src/main.c.o.d"
+  "build/ParakeetMetroidvania"
+  "build/ParakeetMetroidvania.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ParakeetMetroidvania.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/ParakeetMetroidvania.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ParakeetMetroidvania.
+# This may be replaced when dependencies are built.
